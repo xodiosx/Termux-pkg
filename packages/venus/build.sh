f39922f922df2ecf012b5e231665ddc2d3bbd370
Mody <mohammedaleiwi@gmail.com>
2026-06-10 22:07:38 +0300

    Update build.sh

--- packages/venus/build.sh
+++ packages/venus/build.sh
@@ -9,23 +9,12 @@ TERMUX_PKG_SHA256=b181b668afae817953c84635fac2dc4c2e5786c710b7d225ae215d15674a15
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libdrm, libepoxy, libglvnd, libx11, mesa"
 TERMUX_PKG_BUILD_DEPENDS="xorgproto"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Dplatforms=egl,glx -Ddefault_library=static"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Dplatforms=egl,glx"
 
 termux_step_pre_configure() {
+	# error: using an array subscript expression within 'offsetof' is a Clang extension [-Werror,-Wgnu-offsetof-extensions]
+	# list_for_each_entry_safe(struct vrend_linked_shader_program, ent, &shader->programs, sl[shader->sel->type])
 	CPPFLAGS+=" -Wno-error=gnu-offsetof-extensions"
-	LDFLAGS+=" -static"
-
-	local f
-	for f in $(find "$TERMUX_PKG_SRCDIR" -type f -name "*.c"); do
-		if grep -q "memfd_create" "$f"; then
-			sed -i 's/\bmemfd_create\s*(/termux_memfd_create(/g' "$f"
-			sed -i '1s/^/#include <sys\/syscall.h>\n#include <unistd.h>\nstatic inline int termux_memfd_create(const char *n, unsigned int f) { return syscall(SYS_memfd_create, n, f); }\n/' "$f"
-		fi
-		if grep -q "timespec_get" "$f"; then
-			sed -i 's/\btimespec_get\s*(/termux_timespec_get(/g' "$f"
-			sed -i '1s/^/#include <time.h>\n#ifndef TIME_UTC\n#define TIME_UTC 1\n#endif\nstatic inline int termux_timespec_get(struct timespec *ts, int b) { return clock_gettime(0, ts) == 0 ? b : 0; }\n/' "$f"
-		fi
-	done
 
 	if [[ $TERMUX_ARCH != "arm" ]]; then
 		TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -Dvenus=true"
