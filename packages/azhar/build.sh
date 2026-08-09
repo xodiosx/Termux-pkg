ecac81bed6413591018d71e811ccd3e7dfd34ae7
Mody <mohammedaleiwi@gmail.com>
2026-08-09 20:18:58 +0300

    Update build.sh

--- packages/azhar/build.sh
+++ packages/azhar/build.sh
@@ -1,14 +1,13 @@
 TERMUX_PKG_HOMEPAGE=https://azahar-emu.org/
 TERMUX_PKG_DESCRIPTION="Nintendo 3DS video game console emulator"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_MAINTAINER="@termux-user-repository"
+TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2125.1.3"
 TERMUX_PKG_SRCURL="git+https://github.com/azahar-emu/azahar"
 TERMUX_PKG_SHA256=a6ba40a1033bb6cac783428f3b730ff22aa419e2fb239df548afc1b535cc58cb
-TERMUX_PKG_DEPENDS="jack, opengl, catch2-static, pipewire, qt6-qtbase, libxext, libx11, openssl, cryptopp, spirv-tools, nlohmann-json, libenet, portaudio, boost, fmt, ffmpeg, rapidjson, zstd, libusb"
+TERMUX_PKG_DEPENDS="jack, opengl, pipewire, qt6-qtbase, libxext, libx11, openssl, cryptopp, spirv-tools, nlohmann-json, libenet, portaudio, boost, fmt, ffmpeg, rapidjson, zstd, libusb"
 TERMUX_PKG_BUILD_DEPENDS="cmake, vulkan-headers, spirv-headers, libcpufeatures"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DCMAKE_INSTALL_PREFIX=${TERMUX_PREFIX}
--DENABLE_QT=ON
 "
