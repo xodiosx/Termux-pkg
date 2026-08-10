b1fdddb048fdeba60b38950c7f760b55a1eb8ee3
Mody <mohammedaleiwi@gmail.com>
2026-08-10 16:17:55 +0300

    Remove Vulkan handle compatibility fix from build script
    Removed pre-configuration step for Vulkan handle compatibility.

--- packages/azhar/build.sh
+++ packages/azhar/build.sh
@@ -9,15 +9,10 @@ TERMUX_PKG_SHA256=a6ba40a1033bb6cac783428f3b730ff22aa419e2fb239df548afc1b535cc58
 TERMUX_PKG_DEPENDS="jack, sdl2, opengl, pipewire, qt6-qtbase, qt6-qtmultimedia, libxext, libx11, catch2-static, openssl, cryptopp, spirv-tools, nlohmann-json, libenet, portaudio, boost, fmt, rapidjson, zstd, libusb"
 TERMUX_PKG_BUILD_DEPENDS="cmake, vulkan-headers, spirv-headers, libcpufeatures, robin-map"
 TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_BLACKLISTED_ARCHS="arm, i686"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DCMAKE_INSTALL_PREFIX=${TERMUX_PREFIX}
 -DENABLE_LTO=OFF
 -DENABLE_TESTS=OFF
 "
 
-termux_step_pre_configure() {
-	# Fix VK_NULL_HANDLE incompatibility with std::exchange
-	# Vulkan handles do not accept integer assignment, only nullptr.
-	sed -i 's/VK_NULL_HANDLE/nullptr/g' \
-		"${TERMUX_PKG_SRCDIR}/src/video_core/renderer_vulkan/vk_texture_runtime.h"
-}
