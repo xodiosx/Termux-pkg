5ad65e158f467b3e096e2c222c4e59142f94aa7c
Mody <mohammedaleiwi@gmail.com>
2026-08-10 20:16:59 +0300

    Update build.sh

--- x11-packages/retro/build.sh
+++ x11-packages/retro/build.sh
@@ -7,7 +7,7 @@ TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/libretro/RetroArch/archive/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256="58bf6381dbf4aa0f87a8b41725b7a0f7c231dd9ec1f9a26bd283ec4282305ca7"
 TERMUX_PKG_EXCLUDED_ARCHES="arm i686"
-TERMUX_PKG_DEPENDS="libandroid-shmem, libx11, libxrandr, libxext, freetype, alsa-lib, libpulseaudio, openal-soft, libglvnd, ffmpeg, libsixel, qt6-qtbase, libxcb, vulkan-loader"
+TERMUX_PKG_DEPENDS="libandroid-shmem, libx11, libxrandr, libxext, freetype, alsa-lib, pulseaudio, openal-soft, libglvnd, ffmpeg, libsixel, qt6-qtbase, libxcb, vulkan-loader"
 TERMUX_PKG_BUILD_DEPENDS="pkg-config, cmake, vulkan-headers"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --enable-vulkan
