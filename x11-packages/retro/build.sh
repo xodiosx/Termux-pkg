fbcd82b627861c75fdecffe5463dfac6b4a5a24b
Mody <mohammedaleiwi@gmail.com>
2026-08-11 13:02:59 +0300

    Configure build settings for retro package
    Set TERMUX_PKG_BUILD_IN_SRC to true and remove autogen.sh call.

--- x11-packages/retro/build.sh
+++ x11-packages/retro/build.sh
@@ -9,6 +9,7 @@ TERMUX_PKG_SHA256="504a3a8a6e5861eb43a61be8339f61183e7ea940c1ff68ac2a2f57d35c67f
 TERMUX_PKG_EXCLUDED_ARCHES="arm i686 x86_64"
 TERMUX_PKG_DEPENDS="libandroid-shmem, libx11, libxrandr, libxext, freetype, alsa-lib, pulseaudio, openal-soft, libglvnd, ffmpeg, libsixel, qt6-qtbase, libxcb, vulkan-loader"
 TERMUX_PKG_BUILD_DEPENDS="pkg-config, cmake, vulkan-headers, autoconf, automake, libtool"
+TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --enable-vulkan
 --enable-opengl
@@ -22,6 +23,5 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 "
 
 termux_step_pre_configure() {
-	./autogen.sh
 	LDFLAGS+=" -landroid-shmem"
 }
