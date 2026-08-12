0051ded538363b4905b4ceaac3f4ad6d09bd37ac
Mody <mohammedaleiwi@gmail.com>
2026-08-12 20:47:28 +0300

    Update build.sh

--- x11-packages/retro/build.sh
+++ x11-packages/retro/build.sh
@@ -7,7 +7,7 @@ TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/libretro/RetroArch/archive/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256="504a3a8a6e5861eb43a61be8339f61183e7ea940c1ff68ac2a2f57d35c67f8ff"
 TERMUX_PKG_EXCLUDED_ARCHES="arm i686 x86_64"
-TERMUX_PKG_DEPENDS="libandroid-shmem, libx11, libxrandr, libxext, freetype, alsa-lib, pulseaudio, openal-soft, libglvnd, ffmpeg, libsixel, qt6-qtbase, libxcb, vulkan-loader"
+TERMUX_PKG_DEPENDS="libandroid-shmem, fontconfig, libzip, libx11, libxrandr, libxext, freetype, alsa-lib, pulseaudio, sdl2, sdl2-ttf, libglvnd, ffmpeg, libsixel, zlib, libxcb, vulkan-loader"
 TERMUX_PKG_BUILD_DEPENDS="pkg-config, cmake, vulkan-headers, autoconf, automake, libtool"
 TERMUX_PKG_BUILD_IN_SRC=true
 termux_step_configure() {
@@ -16,10 +16,10 @@ termux_step_configure() {
 	./configure \
 		--prefix="$TERMUX_PREFIX" \
 		--enable-vulkan \
-		--enable-opengl \
-		--enable-opengl_core \
+		--enable-sdl2 \
+		--enable-opengles \
 		--enable-alsa \
 		--enable-pulse \
-		--enable-ffmpeg \
+		--disable-ffmpeg \
 		--enable-x11
 }
