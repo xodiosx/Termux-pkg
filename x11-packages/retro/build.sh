9e21d00df6045810d122a8d73bc4f83069eccb23
Mody <mohammedaleiwi@gmail.com>
2026-08-18 17:51:31 +0300

    Update build.sh

--- x11-packages/retro/build.sh
+++ x11-packages/retro/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://www.retroarch.com/
 TERMUX_PKG_DESCRIPTION="Frontend for emulators, game engines and media players"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.22.2"
+TERMUX_PKG_VERSION="1.19.1"
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/libretro/RetroArch/archive/v${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256="245ef18c8fa8fbd9fbb5eb25cf43e17c6aace2f95c1ed99873cbd794012bb232"
+TERMUX_PKG_SHA256="504a3a8a6e5861eb43a61be8339f61183e7ea940c1ff68ac2a2f57d35c67f8ff"
 TERMUX_PKG_EXCLUDED_ARCHES="arm i686 x86_64"
 TERMUX_PKG_DEPENDS="libandroid-shmem, fontconfig, libzip, libx11, libxrandr, libxext, freetype, alsa-lib, pulseaudio, sdl2, sdl2-ttf, libglvnd, ffmpeg, libsixel, zlib, libxcb, vulkan-loader"
 TERMUX_PKG_BUILD_DEPENDS="pkg-config, cmake, vulkan-headers, autoconf, automake, libtool"
@@ -35,10 +35,21 @@ termux_step_pre_configure() {
 	# Force ALSA to use Termux's headers and library
 	export ALSA_CFLAGS="-I$TERMUX_PREFIX/include/alsa"
 	export ALSA_LIBS="-L$TERMUX_PREFIX/lib -lasound"
+
+	# Patch configure script to use Termux's ALSA paths
+	sed -i \
+		-e 's|/usr/include/alsa|'"$TERMUX_PREFIX"'/include/alsa|g' \
+		-e 's|/usr/local/include/alsa|'"$TERMUX_PREFIX"'/include/alsa|g' \
+		-e 's|/usr/lib|'"$TERMUX_PREFIX"'/lib|g' \
+		configure
+
+	# Replace hardcoded /tmp with Termux's prefix/tmp
+	find . -type f \( -name "*.c" -o -name "*.h" \) -exec \
+		sed -i 's|"/tmp"|"'"$TERMUX_PREFIX"'/tmp"|g' {} +
 }
 
 termux_step_configure() {
-	# Manually run configure to avoid the unwanted --disable-dependency-tracking
+	# Run configure manually – omits the unwanted --disable-dependency-tracking
 	./configure \
 		--prefix="$TERMUX_PREFIX" \
 		--host="$TERMUX_HOST_PLATFORM" \
