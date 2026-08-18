e7a2492dcf9fe75816e59f1b63fa86f0a07f1689
Mody <mohammedaleiwi@gmail.com>
2026-08-19 02:04:43 +0300

    Update build.sh

--- x11-packages/retro/build.sh
+++ x11-packages/retro/build.sh
@@ -6,20 +6,22 @@ TERMUX_PKG_VERSION="1.19.1"
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/libretro/RetroArch/archive/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256="504a3a8a6e5861eb43a61be8339f61183e7ea940c1ff68ac2a2f57d35c67f8ff"
-TERMUX_PKG_EXCLUDED_ARCHES="arm i686 x86_64"
+TERMUX_PKG_EXCLUDED_ARCHES="arm i686 x86_64"   # only aarch64
 TERMUX_PKG_DEPENDS="libandroid-shmem, fontconfig, libzip, libx11, libxrandr, libxext, freetype, alsa-lib, pulseaudio, sdl2, sdl2-ttf, libglvnd, ffmpeg, libsixel, zlib, libxcb, vulkan-loader"
-TERMUX_PKG_BUILD_DEPENDS="pkg-config, cmake, vulkan-headers, autoconf, automake, libtool"
+TERMUX_PKG_BUILD_DEPENDS="pkg-config, cmake, vulkan-headers, autoconf, automake, mesa-dev, libtool"
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_AUTO_UPDATE=false
+
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-ffmpeg
 --enable-vulkan
---enable-opengl
+--disable-opengl
 --enable-opengles
+--enable-egl
 --enable-alsa
 --enable-pulse
 --enable-x11
 --enable-sdl2
+--disable-wayland
 "
 
 termux_step_pre_configure() {
@@ -27,29 +29,19 @@ termux_step_pre_configure() {
 	export PKG_CONFIG="$TERMUX_PREFIX/bin/pkg-config"
 	export PKG_CONFIG_PATH="$TERMUX_PREFIX/lib/pkgconfig:$TERMUX_PREFIX/share/pkgconfig"
 
-	# Add Termux include and library paths
+	# Compiler / linker flags for Termux
 	CFLAGS+=" -I$TERMUX_PREFIX/include"
 	CPPFLAGS+=" -I$TERMUX_PREFIX/include"
 	LDFLAGS+=" -L$TERMUX_PREFIX/lib -landroid-shmem"
 
-	# Force ALSA to use Termux's headers and library
+	# Help ALSA detection (overrides hardcoded paths)
 	export ALSA_CFLAGS="-I$TERMUX_PREFIX/include/alsa"
 	export ALSA_LIBS="-L$TERMUX_PREFIX/lib -lasound"
 
-	# Patch configure script to use Termux's ALSA paths
-	sed -i \
-		-e 's|/usr/include/alsa|'"$TERMUX_PREFIX"'/include/alsa|g' \
-		-e 's|/usr/local/include/alsa|'"$TERMUX_PREFIX"'/include/alsa|g' \
-		-e 's|/usr/lib|'"$TERMUX_PREFIX"'/lib|g' \
-		configure
-
-	# Replace hardcoded /tmp with Termux's prefix/tmp
-	find . -type f \( -name "*.c" -o -name "*.h" \) -exec \
-		sed -i 's|"/tmp"|"'"$TERMUX_PREFIX"'/tmp"|g' {} +
 }
 
 termux_step_configure() {
-	# Run configure manually – omits the unwanted --disable-dependency-tracking
+	# Run configure manually to avoid the unwanted --disable-dependency-tracking
 	./configure \
 		--prefix="$TERMUX_PREFIX" \
 		--host="$TERMUX_HOST_PLATFORM" \
