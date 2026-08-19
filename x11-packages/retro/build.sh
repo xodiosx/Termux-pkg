855f7e135c3b0e30d06dbcf8c39bebbdae05b8d4
Mody <mohammedaleiwi@gmail.com>
2026-08-19 17:17:54 +0300

    Update build script for RetroArch package
    Removed ALSA dependencies and adjusted compiler flags for Android compatibility.

--- x11-packages/retro/build.sh
+++ x11-packages/retro/build.sh
@@ -7,8 +7,8 @@ TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/libretro/RetroArch/archive/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256="504a3a8a6e5861eb43a61be8339f61183e7ea940c1ff68ac2a2f57d35c67f8ff"
 TERMUX_PKG_EXCLUDED_ARCHES="arm i686 x86_64"
-TERMUX_PKG_DEPENDS="libandroid-shmem, fontconfig, libzip, libx11, libxrandr, libxext, freetype, alsa-lib, pulseaudio, sdl2, sdl2-ttf, libglvnd, ffmpeg, libsixel, zlib, libxcb, vulkan-loader"
-TERMUX_PKG_BUILD_DEPENDS="pkg-config, cmake, vulkan-headers, autoconf, automake, mesa-dev, libtool"
+TERMUX_PKG_DEPENDS="libandroid-shmem, fontconfig, libzip, libx11, libxrandr, libxext, freetype, pulseaudio, sdl2, sdl2-ttf, libglvnd, ffmpeg, libsixel, zlib, libxcb, vulkan-loader"
+TERMUX_PKG_BUILD_DEPENDS="pkg-config, cmake, vulkan-headers, autoconf, automake, libtool"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
@@ -17,7 +17,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-opengl
 --enable-opengles
 --enable-egl
---enable-alsa
+--disable-alsa
 --enable-pulse
 --enable-x11
 --enable-sdl2
@@ -29,15 +29,14 @@ termux_step_pre_configure() {
 	export PKG_CONFIG="$TERMUX_PREFIX/bin/pkg-config"
 	export PKG_CONFIG_PATH="$TERMUX_PREFIX/lib/pkgconfig:$TERMUX_PREFIX/share/pkgconfig"
 
-	# Compiler / linker flags for Termux
-	CFLAGS+=" -I$TERMUX_PREFIX/include"
-	CPPFLAGS+=" -I$TERMUX_PREFIX/include"
+	# Compiler / linker flags – include _ANDROID_ for Android‑specific code paths
+	CFLAGS+=" -I$TERMUX_PREFIX/include -D_ANDROID_"
+	CPPFLAGS+=" -I$TERMUX_PREFIX/include -D_ANDROID_"
 	LDFLAGS+=" -L$TERMUX_PREFIX/lib -landroid-shmem"
 
-	# Help ALSA detection (overrides hardcoded paths)
-	export ALSA_CFLAGS="-I$TERMUX_PREFIX/include/alsa"
-	export ALSA_LIBS="-L$TERMUX_PREFIX/lib -lasound"
-
+	# Replace hardcoded /tmp with Termux's tmp
+	find . -type f \( -name "*.c" -o -name "*.h" \) -exec \
+		sed -i 's|"/tmp"|"'"$TERMUX_PREFIX"'/tmp"|g' {} +
 }
 
 termux_step_configure() {
